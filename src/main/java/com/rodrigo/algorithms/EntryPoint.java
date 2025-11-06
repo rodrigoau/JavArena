07dc79cc03bc93664c967726fd09d8166130e25e
Rodrigo Ayala <ids.ayala@gmail.com>
2025-11-06 17:48:59 -0600

    new file

---
+++ src/main/java/com/rodrigo/algorithms/EntryPoint.java
@@ -0,0 +1,7 @@
+package com.rodrigo.algorithms;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Algorithms Package!");
+    }
+}
